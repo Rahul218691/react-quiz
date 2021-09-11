f90b861edf7f2917f8aa0d04d09e7bf4e91cbfd8
Rahul218691 <rm218691@gmail.com>
2021-09-11 13:18:53 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://rahul-resume.netlify.app/"
+        style={{ cursor: "pointer" }}
+      >
+        Rahul Guptha
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
